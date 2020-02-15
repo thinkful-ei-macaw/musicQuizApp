8197832b5e26dde4aedb2351ff21ef67b3b4832a
Andrew Durrant <andrew.durrant75@gmail.com>
2020-02-14 17:35:38 -0700

    built out several functions in js file

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
-
+/*eslint-env jquery*/
 /**
  * Example store structure
  */
-const store = {
+const STORE = {
   // 5 or more questions are required
   questions: [
     {
@@ -27,6 +27,7 @@ const store = {
       correctAnswer: '2019'
     }
   ],
+  quizStarted: false,
   questionNumber: 0,
   score: 0
 };
@@ -44,8 +45,79 @@ const store = {
  *
  */
 
+/********** TEMPLATE GENERATION FUNCTIONS **********/
+// These functions return HTML templates
 
-//  SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING
+function generateStartPage() {
+  // return start page html
+  return `
+  <section class="page" id="startPage">
+    <div>
+      <h1>Flex Your Music Knowledge</h1>
+      <h2>Are you ready to rock, juke, jump and jive?</h2>
+    </div>
+    <button id="startBtn">Start Music Quiz</button>
+  </section>`;
+}
+
+function generateAnswers() {
+  let answersHtml = '';
+  STORE.questions[STORE.questionNumber].answers.forEach((answer, i) => {
+    answersHtml += `
+    <span>
+      <input type="radio" id="answer${i}" name="answer${i}" value=${answer}>
+      <label for="answer${i}">${answer}</label><br>
+    </span>
+    `;
+  });
+  return answersHtml;
+}
+
+function generateQuestionPage() {
+  return `
+  <section id="question-page">
+      <article class="status">
+        <span>Question #:</span>
+        <span>Current Score</span>
+      </article>
+      <h3 class="question">${STORE.questions[STORE.questionNumber].question}</h3>
+      <form action="" class="answers">
+        ${generateAnswers()}
+        <button class="submit">SUBMIT</button>
+      </form>
+    </section>`;
+}
 
 
- ******** 
+/********** RENDER FUNCTION(S) **********/
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
+function render() {
+  // if STORE.quizStarted === false then call generateStartPage();
+  if(STORE.quizStarted === false) {
+    $( 'main' ).html(generateStartPage());
+    handleStartQuiz();
+  } else if(STORE.quizStarted === true) {
+    // display question page
+    $( 'main' ).html(generateQuestionPage());
+  }
+
+}
+
+
+/********** EVENT HANDLER FUNCTIONS **********/
+// These functions handle events (submit, click, etc)
+
+function handleStartQuiz() {
+  // add an event listener to start Music Quiz button
+  $( 'main' ).on('click', '#startBtn', function(e) {
+    // change quizStarted to true
+    STORE.quizStarted = true;
+    // --> call render function
+    render();
+  });
+}
+
+
+
+
+$(render());
